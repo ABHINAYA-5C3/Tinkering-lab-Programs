01c00f3d12cfc58d10c65232fb3f8d10f35e0797
ABHINAYA-5C3 <23221a05c3@bvcgroup.in>
2025-11-05 21:58:27 +0530

    4a.dart

---
+++ 4a.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+void main() =>runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+// ---------------- Home Page ----------------
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondPage()),
+            );
+          },
+          child: const Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
+// ---------------- Second Page ----------------
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Go back to Home Page
+            Navigator.pop(context);
+          },
+          child: const Text('Back to Home Page'),
+        ),
+      ),
+    );
+  }
+}
